217cbaa600936466ddf4e94d1cc7139e739fac75
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-27 17:12:52 +0530

    Added: code to calculate fare of multiple fares

--- src/test/java/com/service/cab_invoice_generator/CabServiceGeneratorTest.java
+++ src/test/java/com/service/cab_invoice_generator/CabServiceGeneratorTest.java
@@ -5,16 +5,45 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class CabServiceGeneratorTest {
-
+	/**
+	 * test case for calculating total fare
+	 */
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
 		double distance = 2.0;
 		int time = 5;
 		CabServiceGenerator cabServiceGenerator = new CabServiceGenerator();
-		double fare = cabServiceGenerator.calculateFare(distance,time);
+		double fare = cabServiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(25.0, fare);
-		
-		
+
+	}
+
+	/**
+	 * test case total fare is less than minimum fare should return minimum fare
+	 */
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMiniumumFare() {
+		double distance = 0.1;
+		int time = 2;
+		CabServiceGenerator cabServiceGenerator = new CabServiceGenerator();
+		double fare = cabServiceGenerator.calculateFare(distance, time);
+		System.out.println(fare);
+		Assert.assertEquals(5.0, fare);
+	}
+	/**
+	 * test case to calculate total fare for multiple rides
+	 */
+	@Test
+	public void givenMultipleRides_ShouldReturnAggregateTotalFare() {
+		CabServiceGenerator cabServiceGenerator = new CabServiceGenerator();
+		Ride[] rides = { new Ride(2.0,5),
+				         new Ride(0.1,1),
+						};
+		double fare = cabServiceGenerator.calculateFare(rides);
+		System.out.println(fare);
+		Assert.assertEquals(30.0, fare);
 	}
 	
+	
+
 }
